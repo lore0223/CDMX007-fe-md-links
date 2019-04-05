203e976651902568990464e764ed423f53da7081
lore0223 <loremma23@gmail.com>
2019-04-04 22:56:27 -0600

    empecé con los test pasa uno inyente hacer fetch para validar los links pero aun no funciona

--- read.js
+++ read.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const fs      = require('fs');
 const mdlinks = require('./mdlinks');
-const path = require('path');
+const path    = require('path');
+
 
 const readDir = () => {
     fs.readdir('./', (err, files) => {
@@ -13,26 +14,17 @@ const readDir = () => {
                   if (err) {
                     console.log(err);
                   } else {
-                    mdlinks.getLinks(data);
-                  }
-                })
-              }
-            })
-          }
-        })
-      }
-      
-readDir();
+                  mdlinks.getLinks(data);                                                     
+                  
+                }
+              })
+            }
+          })
+           
+        }
+      })
+    }
+   
+    
 
-        // const readFile = () => {
-        //     fs.readFile('./README.md','utf-8', (err, data) => {
-        //   if (err){
-        //     console.log(err);
-        //   }else{
-        //   mdlinks.getLinks(data);
-        //   }
-        // })
-        // };
-        // readFile()
-
-        module.exports.readDir= readDir;
+        module.exports= readDir;
